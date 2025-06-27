924cb45342708820769910ecd2b35f58a4f4804f
root <basics.314159@gmail.com>
2025-06-27 09:52:44 +0000

    Commit from GDrive-Colab (default msg)

---
+++ code/utils/my_utils.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as pyplot
+
+
+def utils_hello_world(msg="Hello!"):
+    print(f"You have a msg rom my_utils.py/utils_hello_world(msg)")
+    print(f"msg:\n{msg}")
